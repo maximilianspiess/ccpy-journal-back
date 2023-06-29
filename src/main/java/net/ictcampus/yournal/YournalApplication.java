e01e44e1b557e6e195e59e9400404cde11f2b057
Maximilian Spiess <m.spiess.henning@gmail.com>
2023-06-29 10:16:05 +0200

    initial commit

---
+++ src/main/java/net/ictcampus/yournal/YournalApplication.java
@@ -0,0 +1,13 @@
+package net.ictcampus.yournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ictcampus/yournal/YournalApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ictcampus.yournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
